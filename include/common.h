cee401ab9901c1e037ba540b6514dc379929a023
Degra02 <80328248+Degra02@users.noreply.github.com>
2026-03-27 13:54:31 +0100

    add args parse

--- include/common.h
+++ include/common.h
@@ -2,6 +2,10 @@
 #define COMMON_H
 
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
 /// @brief  Objective functions (taken from the paper)
 typedef enum {
@@ -20,7 +24,7 @@ typedef struct {
   uint32_t nd;
 
   // maximum stages
-  uint32_t kmax;
+  uint32_t k_max;
 
   // number of points in the local search of each stage
   uint32_t m;
@@ -40,5 +44,34 @@ typedef struct {
   ObjectiveFunction obj;
   uint64_t seed;
 } SSOConfig;
+
+
+typedef struct {
+    const char *name;
+    ObjectiveFunction value;
+} ObjEntry;
+
+static const ObjEntry obj_registry[] = {
+    {"rastrigin", OBJ_RASTRIGIN},
+    {"griewangk",  OBJ_GRIEWANGK},
+    {"schaffer",   OBJ_SCHAFFER},
+    {NULL, 0}
+};
+
+static struct option long_opts[] = {
+    {"np",    required_argument, 0, 1},
+    {"nd",    required_argument, 0, 2},
+    {"k_max",  required_argument, 0, 3},
+    {"m",     required_argument, 0, 4},
+    {"n_k",   required_argument, 0, 5},
+    {"a_k",   required_argument, 0, 6},
+    {"b_k",   required_argument, 0, 7},
+    {"seed",  required_argument, 0, 8},
+    {"obj", required_argument, 0, 9},
+    {0, 0, 0, 0}
+};
+
+ObjectiveFunction lookup_obj(const char *name);
+void parse_args(int argc, char **argv, SSOConfig *cfg);
 
 #endif
--- src/common.c
+++ src/common.c
@@ -1,1 +1,32 @@
 #include "common.h"
+
+ObjectiveFunction lookup_obj(const char *name) {
+    for (size_t i = 0; obj_registry[i].name != NULL; ++i)
+        if (strcmp(obj_registry[i].name, name) == 0)
+            return obj_registry[i].value;
+
+    fprintf(stderr, "Unknown objective: '%s'. Available:\n", name);
+    for (size_t i = 0; obj_registry[i].name != NULL; ++i)
+        fprintf(stderr, "  %s\n", obj_registry[i].name);
+    exit(1);
+}
+
+void parse_args(int argc, char **argv, SSOConfig *cfg) {
+    int opt, idx;
+    while ((opt = getopt_long(argc, argv, "", long_opts, &idx)) != -1) {
+        switch (opt) {
+            case 1: cfg->np   = (uint32_t)atoi(optarg); break;
+            case 2: cfg->nd   = (uint32_t)atoi(optarg); break;
+            case 3: cfg->k_max = (uint32_t)atoi(optarg); break;
+            case 4: cfg->m    = (uint32_t)atoi(optarg); break;
+            case 5: cfg->n_k  = atof(optarg); break;
+            case 6: cfg->a_k  = atof(optarg); break;
+            case 7: cfg->b_k  = atof(optarg); break;
+            case 8: cfg->seed = (uint64_t)strtoull(optarg, NULL, 10); break;
+            case 9: cfg->obj = lookup_obj(optarg); break;
+            default:
+                fprintf(stderr, "Unknown option\n");
+                exit(1);
+        }
+    }
+}
